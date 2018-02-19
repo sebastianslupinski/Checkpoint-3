cedf880eeb973953b4c0cb97e5b994339263fd88
sebastianslupinski <bezerkey@gmail.com>
2018-02-19 10:26:22 +0100

    implement model TypeBook and Publisher

---
+++ src/Model/Publisher.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Publisher {
+
+    private String publisherId;
+    private String name;
+    private String city;
+    private String country;
+
+    public Publisher(String publisherId, String name, String city, String country){
+        this.publisherId = publisherId;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+    }
+}
---
+++ src/Model/Typebook.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Typebook {
+
+    private int typeId;
+    private String type;
+
+    public Typebook(int typeId, String type){
+        this.typeId = typeId;
+        this.type = type;
+    }
+}
